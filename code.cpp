c6b9c03b8e41d60945352804695bc5f37874d93f
Saurav-bit <56034679+Saurav-bit@users.noreply.github.com>
2021-10-02 23:59:17 +0530

    modified dp

--- code.cpp
+++ code.cpp
@@ -1,27 +1,42 @@
+// C++ program for nth Catalan Number
 #include <iostream>
 using namespace std;
- 
-// A recursive function to find nth catalan number
+
+// Returns value of Binomial Coefficient C(n, k)
+unsigned long int binomialCoeff(unsigned int n,
+								unsigned int k)
+{
+	unsigned long int res = 1;
+
+	// Since C(n, k) = C(n, n-k)
+	if (k > n - k)
+		k = n - k;
+
+	// Calculate value of [n*(n-1)*---*(n-k+1)] /
+	// [k*(k-1)*---*1]
+	for (int i = 0; i < k; ++i) {
+		res *= (n - i);
+		res /= (i + 1);
+	}
+
+	return res;
+}
+
+// A Binomial coefficient based function to find nth catalan
+// number in O(n) time
 unsigned long int catalan(unsigned int n)
 {
-    // Base case
-    if (n <= 1)
-        return 1;
- 
-    // catalan(n) is sum of
-    // catalan(i)*catalan(n-i-1)
-    unsigned long int res = 0;
-    for (int i = 0; i < n; i++)
-        res += catalan(i)
-            * catalan(n - i - 1);
- 
-    return res;
+	// Calculate value of 2nCn
+	unsigned long int c = binomialCoeff(2 * n, n);
+
+	// return 2nCn/(n+1)
+	return c / (n + 1);
 }
- 
+
 // Driver code
 int main()
 {
-    for (int i = 0; i < 10; i++)
-        cout << catalan(i) << " ";
-    return 0;
+	for (int i = 0; i < 10; i++)
+		cout << catalan(i) << " ";
+	return 0;
 }
